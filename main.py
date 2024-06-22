4a4e7124ac5b9d48d1a7470982882ff71e1fc383
Mitch Yeo <41198624+ZenovaLogic@users.noreply.github.com>
2024-06-22 11:27:31 +0100

    Replace txt with json, add search function, error handling
    Now using json format instead of txt to save the data. Can now search the data for a specific website and return saved email/password for that site. Some error handling added.

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ from tkinter import *
 from tkinter import messagebox
 from random import randint, choice, shuffle
 import pyperclip
+import json
+
+# ---------------------------- WRITE TO JSON FILE ------------------------------- #
+def dump_to_file(data):
+    with open("data.json", mode="w") as data_file:
+        json.dump(data, data_file, indent=4)
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_pass():
@@ -28,20 +34,44 @@ def save():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password,
+        }
+    }
 
     if len(website) == 0 or len(password) == 0 or len(email) == 0:
         messagebox.showinfo(title="Oops!", message="Please don't leave any fields empty!")
     else:
-        is_ok = messagebox.askokcancel(title=website, message=f"Details entered:\n"
-                                                              f"Email: {email}\n"
-                                                              f"Password: {password}\n"
-                                                              f"Is it ok to save?")
-        if is_ok:
-            with open("data.txt", mode="a") as data:
-                data.write(f"{website} | {email} | {password}\n")
-                website_entry.delete(0, END)
-                password_entry.delete(0, END)
+        try:
+            with open("data.json", mode="r") as data_file:
+                data = json.load(data_file)
+        except FileNotFoundError:
+            dump_to_file(new_data)
+        else:
+            data.update(new_data)
+            dump_to_file(data)
+        finally:
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
 
+# ---------------------------- FIND PASSWORD ------------------------------- #
+def find_password():
+    website = website_entry.get()
+
+    try:
+        with open("data.json", mode="r") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showerror(title="Error", message="No data file found!")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Email: {email}\nPassword: {password}")
+        else:
+            messagebox.showerror(title="Error", message=f"No data found for {website}.")
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -59,23 +89,25 @@ email_label = Label(text="Email/Username:")
 password_label = Label(text="Password:")
 
 # Entries
-website_entry = Entry(width=38)
+website_entry = Entry(width=23)
 email_entry = Entry(width=38)
 password_entry = Entry(width=23)
 
 # Buttons
 gen_pass_button = Button(text="Generate Password", font=("", 9), width=12, command=generate_pass)
 add_button = Button(text="Add", width=36, font=("", 9), command=save)
+search_button = Button(text="Search", width=12, font=("", 9), command=find_password)
 
 # Place all objects on grid
 website_label.grid(column=0, row=1)
 email_label.grid(column=0, row=2)
 password_label.grid(column=0, row=3)
-website_entry.grid(column=1, row=1, columnspan=2)
+website_entry.grid(column=1, row=1)
 website_entry.focus()
 email_entry.grid(column=1, row=2, columnspan=2)
 password_entry.grid(column=1, row=3)
 gen_pass_button.grid(column=2, row=3)
 add_button.grid(column=1, row=4, columnspan=2)
+search_button.grid(column=2, row=1)
 
 window.mainloop()
